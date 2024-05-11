15b932c75d5dad9829c9c3ab5ed78442ef17bd51
Frank sharpe <145439826+franksharpe@users.noreply.github.com>
2024-05-11 10:08:12 +0100

    works

--- bandwith tracker.py
+++ bandwith tracker.py
@@ -7,28 +7,26 @@ def get_net_speed():
     time.sleep(1)
     new_stat = psutil.net_io_counters()
     
-    
     bytes_sent = (new_stat.bytes_sent - old_stat.bytes_sent) / 1024
     bytes_recv = (new_stat.bytes_recv - old_stat.bytes_recv) / 1024
     
-    return bytes_sent , bytes_recv
-
+    return bytes_sent, bytes_recv
 
 def update_gui():
     sent, recv = get_net_speed()
     
-    upload_speed_label.config(text= f"Upload: {sent:.2f} KB/s")
-    download_speed_label.config(text= f"Download: {recv:.2f} KB/s")
+    upload_speed_label.config(text=f"Upload: {sent:.2f} KB/s")
+    download_speed_label.config(text=f"Download: {recv:.2f} KB/s")
     
     root.after(1000, update_gui)
 
 root = tk.Tk()  
 root.title("Bandwidth monitor")
 
-upload_speed_label = tk.Label(root,text="Upload: 0 KB/s")
+upload_speed_label = tk.Label(root, text="Upload: 0 KB/s")
 upload_speed_label.pack()
 
-download_speed_label = tk.Label(root,text="Download: 0 KB/s")
+download_speed_label = tk.Label(root, text="Download: 0 KB/s")
 download_speed_label.pack()
 
 update_gui()
